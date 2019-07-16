cffd6994d53dacf937b2742e46c707b56a83ba48
Chris Rrapi <toadawes12@gmail.com>
2019-07-16 06:19:40 -0400

    use ACT LED instead of GPIO

--- pihole-led.sh
+++ pihole-led.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
-###########
-### This pihole-led script is built for the odroid c1+ using wiringPi here:
-### https://github.com/hardkernel/wiringPi
-### Built for pihole log's however you can modify as needed of course.
-### I use header pin 9 for the ground and pin 11 for gpio.
-### header 11 is equal to export gpio=88 or wiringPi pin=0 as used below.
-###########
-### Version: 		0.1
-### Author: 		Daniel Stinebaugh
-### Attribution:	http://www.stinebaugh.info/get-led-alerts-for-each-blocked-ad-using-pi-hole/
-### Date: 			03/19/2016
-### License: 		Copyleft. Enjoy!
-###########
-pin=0
-gpio mode $pin out
-# Defaults the pin to low when starting
-gpio write $pin 0
-tailf /var/log/pihole.log | while read INPUT
+echo none | sudo tee /sys/class/leds/led0/trigger
+echo 1 | sudo tee /sys/class/leds/led0/brightness
+
+tail -f /var/log/pihole.log | while read INPUT
 do
    if [[ "$INPUT" == *"/etc/pihole/gravity.list"* ]]; then
-       gpio write $pin 1
+       echo 0 | sudo tee /sys/class/leds/led0/brightness
        sleep 0.2
-       gpio write $pin 0
-#       echo "another ad bites the dust!"
-       sleep 0.1
+       echo 1 | sudo tee /sys/class/leds/led0/brightness
+       sleep 0.2
     fi
 done
